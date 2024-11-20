60b99cd3891299c0fead3baed6a33cbb0277c236
Lexion <senaalfarabi31@gmail.com>
2024-11-20 15:03:32 +0700

    Mengubah codingan Arduino_Terbaru

--- Arduino_Terbaru/Arduino_Terbaru.ino
+++ Arduino_Terbaru/Arduino_Terbaru.ino
@@ -1,45 +1,108 @@
-int proxy = 2;
-int capacitive = 3;
-int inductive = 5;
+#include <Servo.h>
 
-//int servo_buka = 5;
-//int servo_i = 6; 
-//int servo_c = 9;
+Servo servo_buka;
+Servo servo_inductive;
+Servo servo_capacitive;
 
-void setup() {
-  Serial.begin(9600);
+const int trig_pin = 12;
+const int echo_pin = 13;
 
-  pinMode(proxy, INPUT);
-  pinMode(inductive, INPUT);
-  pinMode(capacitive, INPUT);
+const int proxy = 2;
+const int capacitive = 4;
+const int inductive = 3;
 
-//  pinMode(servo_p, OUTPUT);
-//  pinMode(servo_i, OUTPUT);
-//  pinMode(servo_c, OUTPUT);
+float timing = 0.00;
+float distance = 0.00;
+
+//unsigned long startTime;
+//boolean timerStarted = false;
+//const unsigned long DETECTION_PERIOD = 5000; 
+boolean objectDetected = false;
+
+const unsigned long interval = 50;
+const unsigned long total_checks = 100;
+
+int cek;
+
+void setup(){
+pinMode(proxy, INPUT);
+pinMode(inductive, INPUT);
+pinMode(capacitive, INPUT);
+
+pinMode(trig_pin, OUTPUT);
+pinMode(echo_pin, INPUT);
+
+digitalWrite(trig_pin, LOW);
+servo_buka.attach(5);
+servo_inductive.attach(6);
+servo_capacitive.attach(9);
+
+Serial.begin(9600);
+
+servo_buka.write(0);
+
 }
 
-void loop() {
-  if(digitalRead(proxy) == 0){
+void loop(){
+  servo_inductive.write(90);
+  servo_capacitive.write(90);
+  Deteksi();
+  if(cek == 1){
+    Sortir();
+  }
+  delay(1000);
+}
+
+void Deteksi(){
+digitalWrite(trig_pin, LOW);
+delay(20);
+digitalWrite(trig_pin, HIGH);
+delay(40);
+digitalWrite(trig_pin, LOW);
+
+timing = pulseIn(echo_pin, HIGH);
+distance = (timing * 0.034) / 2;  
+Serial.print("Distance: ");
+Serial.print(distance);
+Serial.println("cm");
+
+if (distance <= 20) {
+servo_buka.write(90);
+cek = 1;
+} else {
+servo_buka.write(0);
+cek = 0;
+}
+  
+delay(1000);
+}
+
+void Sortir(){
+  objectDetected = false;
+
+  for(int i = 0 ; i <= total_checks ; i++){
+   if(digitalRead(inductive) == 0){
     Serial.println(String(1));
+    servo_inductive.write(0);
+    objectDetected = true;
+    break;
+  }
+   else if (i == total_checks){
+    servo_inductive.write(180);
+    break;
+   }
+  delay(interval);  
+  }
+  
+  if(digitalRead(capacitive) == 1){
+    Serial.println(String(2));
   }
 
-//  else if(digitalRead(capacitive) == 0){
-//    Serial.println(String(2));
-//  }
-
-  else if(digitalRead(inductive) == 0){
+  else if(digitalRead(proxy) == 0){
     Serial.println(String(3));
   }
-
   delay(1000);
-//  if(sensor == 0){
-//    Serial.println(String(1));  
-//  }
-//   delay(1000);
-//  Serial.println(String(2));
-//  delay(1000);
-//  Serial.println(String(3));
-//  delay(1000);
-  // put your main code here, to run repeatedly:
+
 
 }
+  
